1d1a3891d06f6277fa51c392203fee5526f5ba63
Andre Wolff <awolff@merrick.com>
2023-08-16 13:58:01 -0600

    EOD Commit

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 
 var file_name = "res://Data/contracts.json"
-
+var data = null
+var selected_contract = null
 
 func _ready():
 	var global = $/root/Global
@@ -13,18 +14,23 @@ func _ready():
 	var file = FileAccess.open(file_name, FileAccess.READ)
 	var json_text = file.get_as_text()
 	file.close()
-	var data = JSON.parse_string(json_text)
+	data = JSON.parse_string(json_text)
 	if data:
 		for contract in data:
 			var item_text = contract["name"]
 			
-			$contracts_panel/ItemList.add_item(item_text)
+			$contracts_panel/contract_list.add_item(item_text)
 
 
 
 func _on_item_list_item_selected(index):
-	print(index, "selected")
-
-
-func _on_item_list_item_activated(index):
-	print(index, "activated")
+	print(index, "selected", data[index])
+	var contract = data[index]
+	var details = "Name: %s\n
+				Description: %s\n
+				Difficulyt: %s\n
+				Reward: $%d" % [contract["name"],
+				contract["description"],
+				contract["difficulty"],
+				contract["reward"]]
+	$contracts_panel/contract_details.text = details
